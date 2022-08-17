a3a2a8f736b19a1e478c0b47e82efd18416f5bbe
Dylan Fontaine <dylanfontaine@live.ca>
2022-08-16 20:50:41 -0700

    Update bundle

--- bundles/provider.umd.js
+++ bundles/provider.umd.js
@@ -2,7 +2,7 @@
 {
   "name": "My Provider Package",
   "id": "com.syncler.kosmos.mypackage",
-  "version": 6,
+  "version": 7,
   "classPath": "myProvider.MyPackage",
   "permaUrl": "https://raw.githubusercontent.com/dylf/kosmos-provider/main/bundles/provider.umd.js"
 } 
@@ -89,6 +89,18 @@
                     sources.push({
                         ...defaults,
                         host: 'caught',
+                    });
+                    sources.push({
+                        ...defaults,
+                        host: typeof crypto,
+                    });
+                    sources.push({
+                        ...defaults,
+                        host: typeof window,
+                    });
+                    sources.push({
+                        ...defaults,
+                        host: typeof globalThis,
                     });
                     resolve(sources);
                 });
