497c74c6160d39385258e5e8cac699ec935d0bd7
Dylan Fontaine <dylanfontaine@live.ca>
2022-09-14 21:14:24 -0700

    Update bundle

--- bundles/provider.umd.js
+++ bundles/provider.umd.js
@@ -2,7 +2,7 @@
 {
   "name": "My Provider Package",
   "id": "com.syncler.kosmos.mypackage",
-  "version": 9,
+  "version": 10,
   "classPath": "myProvider.MyPackage",
   "permaUrl": "https://raw.githubusercontent.com/dylf/kosmos-provider/main/bundles/provider.umd.js"
 } 
@@ -14,21 +14,27 @@
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.myProvider = {}));
 })(this, (function (exports) { 'use strict';
 
-    const getEncryptedKey = async (data) => {
-        let key = await window.crypto.subtle.importKey('raw', new TextEncoder().encode('37911490979715163134003223491201'), 'AES-CBC', true, ['encrypt', 'decrypt']);
-        return await window.crypto.subtle.encrypt({
-            name: 'AES-CBC',
-            iv: new TextEncoder().encode('3134003223491201'),
-        }, key, new TextEncoder().encode(data));
-    };
-    const decryptTheScript = async (script) => {
-        let key = await window.crypto.subtle.importKey('raw', new TextEncoder().encode('37911490979715163134003223491201'), 'AES-CBC', true, ['encrypt', 'decrypt']);
-        const decrypted = await window.crypto.subtle.decrypt({
-            name: 'AES-CBC',
-            iv: new TextEncoder().encode('3134003223491201'),
-        }, key, Buffer.from(script));
-        return new TextDecoder().decode(decrypted);
-    };
+    async function getAnimeData(httpClient, title) {
+        const result = await httpClient.get(`https://consumet-api.herokuapp.com/anime/gogoanime/${title}`);
+        const list = Array.isArray(result.data?.results)
+            ? result.data.results
+            : [];
+        if (list.length < 1) {
+            throw Error();
+        }
+        return list[0];
+    }
+    async function getAnimeUrls(httpClient, animeId, episode) {
+        const subAnime = httpClient.get(`https://consumet-api.herokuapp.com/anime/gogoanime/watch/${animeId}-episode-${episode}`);
+        const dubAnime = httpClient.get(`https://consumet-api.herokuapp.com/anime/gogoanime/watch/${animeId}-dub-episode-${episode}`);
+        const [subResult, dubResult] = await Promise.all([subAnime, dubAnime]);
+        const subSources = subResult.data;
+        const dubSources = dubResult.data;
+        return {
+            subSources,
+            dubSources,
+        };
+    }
 
     class Provider {
         metadata;
@@ -44,74 +50,48 @@
         search(env, request) {
             return new Promise((resolve, reject) => {
                 let sources = [];
-                const defaults = {
-                    providerName: 'false',
-                    name: 'thefilename.mkv',
-                    resolved: true,
-                    quality: '4k',
-                    url: 'https://github.com/ietf-wg-cellar/matroska-test-files/blob/master/test_files/test1.mkv?raw=true',
-                };
                 if (request.movie) {
                     reject();
                     return;
                 }
-                const { show: item, episodeNumber, seasonNumber } = request.episode;
-                sources.push({
-                    ...defaults,
-                    host: JSON.stringify(Object.keys(this)),
-                });
-                sources.push({
-                    ...defaults,
-                    host: JSON.stringify(item.titles.main),
-                });
-                getEncryptedKey('foo')
-                    .then((enc) => {
-                    sources.push({
-                        ...defaults,
-                        host: '55',
-                    });
-                    const byteArrayToBase64 = (array) => {
-                        let u_binary = '';
-                        let u_bytes = new Uint8Array(array);
-                        let u_len = u_bytes.byteLength;
-                        for (let i = 0; i < u_len; i++) {
-                            u_binary += String.fromCharCode(u_bytes[i]);
-                        }
-                        return btoa(u_binary);
-                    };
-                    sources.push({
-                        ...defaults,
-                        host: new TextDecoder().decode(enc),
-                    });
-                    const dec = byteArrayToBase64(enc);
-                    return decryptTheScript(dec);
+                const { show: item, episodeNumber } = request.episode;
+                if (!item.anime) {
+                    reject();
+                    return;
+                }
+                const defaults = {
+                    providerName: 'myprovider',
+                    host: 'GoGoAnime',
+                    resolved: true,
+                };
+                const axios = env.httpClientFactory.createNewInstance();
+                getAnimeData(axios, item.titles.main.title)
+                    .then(({ id }) => {
+                    return getAnimeUrls(axios, id, episodeNumber);
                 })
-                    .then((de) => {
-                    sources.push({
-                        ...defaults,
-                        host: JSON.stringify(de),
+                    .then(({ subSources, dubSources }) => {
+                    subSources.sources.forEach((source) => {
+                        sources.push({
+                            ...defaults,
+                            url: source.url,
+                            quality: source.quality,
+                            subbed: true,
+                            dubbed: false,
+                            headers: subSources.headers,
+                        });
                     });
-                    return resolve(sources);
-                })
-                    .catch(() => {
-                    sources.push({
-                        ...defaults,
-                        host: 'caught',
+                    dubSources.sources.forEach((source) => {
+                        sources.push({
+                            ...defaults,
+                            url: source.url,
+                            quality: source.quality,
+                            subbed: false,
+                            dubbed: true,
+                            headers: dubSources.headers,
+                        });
                     });
-                    sources.push({
-                        ...defaults,
-                        host: typeof crypto,
-                    });
-                    sources.push({
-                        ...defaults,
-                        host: typeof window,
-                    });
-                    sources.push({
-                        ...defaults,
-                        host: typeof globalThis,
-                    });
-                    resolve(sources);
                 });
+                resolve(sources);
             });
         }
     }
